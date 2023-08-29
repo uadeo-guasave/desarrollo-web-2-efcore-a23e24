27159b3f8bbb3db436a38f2bcacfa2d0081afa07
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2023-08-29 08:24:40 -0700

    Docentes actividades
    Ejemplo del proyecto de Damaris, Anahi y Blanca

---
+++ Damaris/Docente.cs
@@ -0,0 +1,9 @@
+namespace HolaMundo.Damaris;
+
+// PascalCase
+class Docente
+{
+    public int NumeroDeEmpleado { get; set; }
+    public string? Nombres { get; set; }
+    public string? Apellidos { get; set; }
+}
